b80bcf5f29a39c472906a5ea90faa46a6695bf54
DimaSurdiy <dsurdiy@ukr.net>
2022-08-26 07:15:48 +0300

    add BrowserRouter & movies api

---
+++ src/services/movies-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'f6301fd4418791e86cee5ab1a6c319ce';
+
+export async function fetchTrending() {
+  const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+  return response.data.results;
+}
+
+export async function fetchMovieById(id) {
+  const response = await axios.get(`movie/${id}?api_key=${API_KEY}`);
+  return response.data;
+}
+
+export async function fetchMovieCast(id) {
+  const response = await axios.get(`movie/${id}/credits?api_key=${API_KEY}`);
+  return response.data.cast;
+}
+
+export async function fetchMovieReviews(id) {
+  const response = await axios.get(`movie/${id}/reviews?api_key=${API_KEY}`);
+  return response.data.results;
+}
